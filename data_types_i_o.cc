a1fecc82b5a9bddc1072f5977808c748edc17ea1
S.I <sheiliyer@hotmail.com>
2022-09-14 23:24:47 -0400

    committing comments

---
+++ data_types_i_o.cc
@@ -0,0 +1,4 @@
+/** Name: Sheil Iyer
+Purpose: Utilizing various tools in C++ including commenting, declaring variables,
+getting user input from keyboard, and generating output to terminal screen. It is also practice for pushing/committing to Github
+*/
